d889bf4d24791064b4e95af016d5501eecdc94e1
Goldzia <ggolda66@gmail.com>
2024-10-08 14:25:17 +0200

    test1

--- helpers/data.js
+++ helpers/data.js
@@ -2,5 +2,4 @@ import 'dotenv/config'
 
 export const baseUrl = "https://demoqa.com"
 export const userId = "12d595ec-edd4-44b7-be4a-1e7d3aa4ebb5"
-export const userName = process.env.SECRET_USERNAME
 export const password = process.env.SECRET_PASSWORD
